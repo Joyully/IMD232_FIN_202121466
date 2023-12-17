167f197e372f51c1c9c1fe440bd57a70646ec144
Joyully <143991481+Joyully@users.noreplay.github.com>
2023-12-18 01:29:42 +0900

    final

---
+++ script/alert.js
@@ -0,0 +1,27 @@
+// Swal.fire(
+//   '고민이 있으신가요?',
+//   '고민을 생각하면서... 볼을 흔들어보세요  그럼 답이 나올지도..?'
+// );
+
+Swal.fire({
+  title: '고민이 있으신가요?',
+  text: '고민을 생각하면서... 볼을 흔들어보세요  그럼 답이 나올지도..? "ENTER"',
+
+  width: 600,
+  icon: 'question',
+});
+
+// Swal.fire({
+//   title:
+//     '고민이 있으신가요? 고민을 생각하면서... 볼을 흔들어보세요  그럼 답이 나올지도..? ',
+//   width: 600,
+//   padding: '1em',
+//   color: '#4F4BDF',
+//   background: 'gainsboro',
+//   backdrop: `
+//       rgba(0,0,123,0.4)
+//     //   url("/images/nyan-cat.gif")
+//     //   left top
+//     //   no-repeat
+//     `,
+// });
